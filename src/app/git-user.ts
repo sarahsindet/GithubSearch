5b28da17d86583d7265e0d8ec4e46fa1009f0508
sarahsindet <sarahsindet@gmail.com>
2020-06-23 21:20:17 +0300

    added a git user class

---
+++ src/app/git-user.ts
@@ -0,0 +1,11 @@
+export class GitUser {
+    login: any;
+    bio: any;
+    name: any;
+    company: any;
+    location: any;
+    followers: any;
+    following: any;
+    created_at: Date;
+    public_repos: any;
+}
