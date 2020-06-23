e00fc3a503ecba87d8a90e23a81173b11b019bcc
sarahsindet <sarahsindet@gmail.com>
2020-06-23 21:09:50 +0300

    creted a date-pipe

--- src/app/date-pipe.pipe.ts
+++ src/app/date-pipe.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    const today: Date = new Date();
+    const todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    let dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInADay = 86400;
+
+    const dateDifferenceSeconds = dateDifference * 0.001;
+    const dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if (dateCounter >= 1 && todayWithNoTime > value) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
 
 }
